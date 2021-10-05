3bca2f101d2a2cea2df6e95309ee32ad0f293dbb
Gopesh Kumar Jangid <gopesh.jangid@gmail.com>
2021-10-05 16:01:32 +0530

    chat

--- components/Chat/index.js
+++ components/Chat/index.js
@@ -1,48 +1,175 @@
-import React ,{useEffect,useState} from 'react';
-import { Widget, addResponseMessage } from 'react-chat-widget';
-import 'react-chat-widget/lib/styles.css';
-import {connect } from "react-redux";
-import {sendMessage , getAllMessages , getLatestMessage } from  "../Modules/Support/redux/action";
+import React, { Component } from 'react';
+import ChatBot from 'react-simple-chatbot';
 
-function App({sendMessage = ()=>{} ,getAllMessages,getLatestMessage,latestMessage , messageList}) {
-  const [chat , setChat] = useState(null);
-  useEffect(() => {
-    addResponseMessage('Welcome to comboware support!');
-    // const interval = setInterval(()=>{
-    //   //getLatestMessage(`?chatId=${chat?.chat_id}&userId=0`)
-    // },2000);
+class Review extends Component {
+  constructor(props) {
+    super(props);
 
-    return ()=>{
-      //clearInterval(interval);
-    }
-  }, []);
+    this.state = {
+      name: '',
+      gender: '',
+      age: '',
+    };
+  }
 
-  useEffect(() =>{
-    addResponseMessage(latestMessage||'');
-    if(!chat){
-      setChat({chat_id : latestMessage?.chat_id ,userId :0})
-    }
-  },[latestMessage])
+  componentWillMount() {
+    const { steps } = this.props;
+    const { name, gender, age } = steps;
 
-  const handleNewUserMessage = (newMessage) => {
-    console.log(`New message incoming! ${newMessage}`);
-    sendMessage({...chat, message : newMessage})
-    // Now send the message throught the backend API
-   // addResponseMessage(response);
-  };
+    this.setState({ name, gender, age });
+  }
 
-  return (
-      <Widget
-        handleNewUserMessage={handleNewUserMessage}
-        //profileAvatar={logo}
-          title="Welcome to comboware"
-          subtitle="Ask your queries here"
-          autofocus={true}
-          sendButtonAlt={true}
-          resizable={true}
-          emojis={true}
-      />
-  );
+  render() {
+    const { name, gender, age } = this.state;
+    return (
+      <div style={{ width: '100%' }}>
+        <h3>Summary</h3>
+        <table>
+          <tbody>
+            <tr>
+              <td>Name</td>
+              <td>{name.value}</td>
+            </tr>
+            <tr>
+              <td>Gender</td>
+              <td>{gender.value}</td>
+            </tr>
+            <tr>
+              <td>Age</td>
+              <td>{age.value}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    );
+  }
 }
 
-export default connect((state) =>({...state?.support}), {getAllMessages, getLatestMessage , sendMessage})(App);
+class SimpleForm extends Component {
+  render() {
+    return (
+      <ChatBot
+      floating={true}
+        steps={[
+          {
+            id: 'welcome',
+            message: 'Hi there, Welcome to Comboware !',
+            trigger: '1',
+          },
+          {
+            id: '1',
+            message: 'What is your name?',
+            trigger: 'name',
+          },
+          {
+            id: 'name',
+            user: true,
+            trigger: '3',
+          },
+          {
+            id: '3',
+            message: 'Hi {previousValue}! What is your email?',
+            trigger: 'email'
+          },
+          {
+            id: 'email',
+            user: true,
+            trigger: 'confirm',
+            validator: (value) => {
+              var reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+              if (!reg.test(value)) {
+                return 'Please enter valid email address.';
+              } 
+              return true;
+            },
+          },
+          {
+            id: 'confirm',
+            message: 'Ok great! how may i help you ?',
+            trigger: 'email',
+            
+          },
+          {
+            id: 'start_chat',
+            user: true,
+            trigger: 'startMessage',
+          },
+          {
+            id: 'startMessage',
+            user: true,
+            trigger: '7',
+            validator: (value) => {
+              if (isNaN(value)) {
+                return 'value must be a number';
+              } else if (value < 0) {
+                return 'value must be positive';
+              } else if (value > 120) {
+                return `${value}? Come on!`;
+              }
+
+              return true;
+            },
+          },
+          {
+            id: '7',
+            message: 'Great! Check out your summary',
+            trigger: 'review',
+          },
+          {
+            id: 'review',
+            component: <Review />,
+            asMessage: true,
+            trigger: 'update',
+          },
+          {
+            id: 'update',
+            message: 'Would you like to update some field?',
+            trigger: 'update-question',
+          },
+          {
+            id: 'update-question',
+            options: [
+              { value: 'yes', label: 'Yes', trigger: 'update-yes' },
+              { value: 'no', label: 'No', trigger: 'end-message' },
+            ],
+          },
+          {
+            id: 'update-yes',
+            message: 'What field would you like to update?',
+            trigger: 'update-fields',
+          },
+          {
+            id: 'update-fields',
+            options: [
+              { value: 'name', label: 'Name', trigger: 'update-name' },
+              { value: 'gender', label: 'Gender', trigger: 'update-gender' },
+              { value: 'age', label: 'Age', trigger: 'update-age' },
+            ],
+          },
+          {
+            id: 'update-name',
+            update: 'name',
+            trigger: '7',
+          },
+          {
+            id: 'update-gender',
+            update: 'gender',
+            trigger: '7',
+          },
+          {
+            id: 'update-age',
+            update: 'age',
+            trigger: '7',
+          },
+          {
+            id: 'end-message',
+            message: 'Thanks! Your data was submitted successfully!',
+            end: true,
+          },
+        ]}
+      />
+    );
+  }
+}
+
+export default SimpleForm;
