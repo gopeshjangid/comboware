a997d3ee2fd6f3b1a0832c6514d6c9d72d23ae5d
Gopesh Kumar Jangid <gopesh.jangid@gmail.com>
2021-10-31 20:47:42 +0530

    new look

--- layouts/AppBar.js
+++ layouts/AppBar.js
@@ -4,21 +4,12 @@ import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import InputBase from '@mui/material/InputBase';
-import Badge from '@mui/material/Badge';
-import MenuItem from '@mui/material/MenuItem';
-import Menu from '@mui/material/Menu';
 import MenuIcon from '@mui/icons-material/Menu';
-import SearchIcon from '@mui/icons-material/Search';
-import AccountCircle from '@mui/icons-material/AccountCircle';
-import MailIcon from '@mui/icons-material/Mail';
-import NotificationsIcon from '@mui/icons-material/Notifications';
 import MoreIcon from '@mui/icons-material/MoreVert';
 import logo from "assets/img/logo.svg";
 import Hidden from "@material-ui/core/Hidden";
 import AdminNavbarLinks from "components/Navbars/AdminNavbarLinks";
-import PushNotification from "components/Notification/PushNotification";
+//import PushNotification from "components/Notification/PushNotification";
 const Search = styled('div')(({ theme }) => ({
   position: 'relative',
   borderRadius: theme.shape.borderRadius,
